c4c8dd07ac718671c2e8f075c267ce5bcbfc5d67
anilshresth <shresthaxxx66@gmail.com>
2020-11-24 08:51:32 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React,{useState} from 'react';
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from './components/Todo';
+import {nanoid } from "nanoid";
+
+
+function App(props) {
+  
+  const [tasks, setTasks] = useState(props.tasks)
+
+
+  function addTask(name){
+   const newTask={id:"todo-"+nanoid(),name:name,completed:false};
+   setTasks([...tasks,newTask]);
+
+  }
+  function deleteTask(id){
+    const remainingTask=tasks.filter(task=>id!=task.id);
+    setTasks(remainingTask);
+  }
+  function editTask(id,newName){
+    const editedTaskList = tasks.map(task => {
+      // if this task has the same ID as the edited task
+        if (id === task.id) {
+          //
+          return {...task, name:newName}
+        }
+        return task;
+      });
+      setTasks(editedTaskList);
+    }
+
+  const taskList=(tasks.map(task=><Todo id={task.id}  name={task.name} completed={task.completed}  key={task.id}  toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask} editTask={editTask}/>)
+  )
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+ 
+
+  const taskNoun=taskList.length==1?'task':'tasks';
+  const headingtext=`${taskList.length} ${taskNoun} remaining`;
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask}/>
+      
+      <div className="filters btn-group stack-exception">
+        
+        
+         <FilterButton/>
+         <FilterButton/>
+         <FilterButton/>
+      </div>
+      <h2 id="list-heading">
+        {headingtext}
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+       
+      </ul>
+    </div>
+  );
+}
+
+
+export default App;
